24bc2f83051535c3fab5395e2da77f126493e7ee
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2023-02-19 11:43:12 -0800

    Add files via upload

---
+++ UniRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniRideApp.swift
+//  UniRide
+//
+//  Created by Jesus Ballesteros on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UniRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Homeview()
+        }
+    }
+}
